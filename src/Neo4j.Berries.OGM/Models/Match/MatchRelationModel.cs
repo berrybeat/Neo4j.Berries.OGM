e8438c9a3126edc0ae81ffa35a9f3a048c76027e
Farhad Nowzari <farhad.nowzari@berrybeat.com>
2024-04-05 17:59:18 +0200

    untyped match relation model added

--- src/Neo4j.Berries.OGM/Models/Match/MatchRelationModel.cs
+++ src/Neo4j.Berries.OGM/Models/Match/MatchRelationModel.cs
@@ -5,23 +5,20 @@ using Neo4j.Berries.OGM.Utils;
 
 namespace Neo4j.Berries.OGM.Models.Match;
 
-internal class MatchRelationModel<TEndNode>(IMatch startMatch, IRelationConfiguration relationConfig, Eloquent<TEndNode> eloquent, int index) : IMatch
-where TEndNode : class
+internal class MatchRelationModel(string endNodeLabel, IMatch startMatch, IRelationConfiguration relationConfig, Eloquent eloquent, int index) : IMatch
 {
     private IMatch StartMatch { get; set; } = startMatch;
-    private string EndNodeLabel => typeof(TEndNode).Name;
     private IRelationConfiguration RelationConfig { get; set; } = relationConfig;
-    private Eloquent<TEndNode> EndNodeEloquent { get; set; } = eloquent;
+    private Eloquent EndNodeEloquent { get; set; } = eloquent;
     public string StartNodeAlias => StartMatch.StartNodeAlias;
     public string EndNodeAlias => $"l{index}";
     public string RelationAlias => $"r{index}";
-    
     public IMatch ToCypher(StringBuilder cypherBuilder)
     {
         if (EndNodeEloquent != null)
-            cypherBuilder.AppendLine($"MATCH ({StartMatch.StartNodeAlias}){RelationConfig.Format(RelationAlias)}({EndNodeAlias}:{EndNodeLabel} WHERE {EndNodeEloquent.ToCypher(EndNodeAlias)})");
+            cypherBuilder.AppendLine($"MATCH ({StartMatch.StartNodeAlias}){RelationConfig.Format(RelationAlias)}({EndNodeAlias}:{endNodeLabel} WHERE {EndNodeEloquent.ToCypher(EndNodeAlias)})");
         else
-            cypherBuilder.AppendLine($"MATCH ({StartMatch.StartNodeAlias}){RelationConfig.Format(RelationAlias)}({EndNodeAlias}:{EndNodeLabel})");
+            cypherBuilder.AppendLine($"MATCH ({StartMatch.StartNodeAlias}){RelationConfig.Format(RelationAlias)}({EndNodeAlias}:{endNodeLabel})");
         return this;
     }
     public Dictionary<string, object> GetParameters()
@@ -29,3 +26,6 @@ where TEndNode : class
         return EndNodeEloquent?.QueryParameters ?? [];
     }
 }
+
+internal class MatchRelationModel<TEndNode>(IMatch startMatch, IRelationConfiguration relationConfig, Eloquent<TEndNode> eloquent, int index) : MatchRelationModel(typeof(TEndNode).Name, startMatch, relationConfig, eloquent, index)
+where TEndNode : class { }
